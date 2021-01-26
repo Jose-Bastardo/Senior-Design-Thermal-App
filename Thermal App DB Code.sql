a9649dfefe422ede5f4ccfbcf7be407aec4ed41c
MajorZap <72284835+MajorZap@users.noreply.github.com>
2021-01-26 10:31:08 -0500

    Added database code

---
+++ Thermal App DB Code.sql
@@ -0,0 +1,21 @@
+CREATE TABLE user(userid INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+               firstname VARCHAR(100), 
+			   lastname VARCHAR(100), 
+			   email VARCHAR(100), 
+			   password VARCHAR(100));
+			   
+CREATE TABLE dat(datid INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+userid int unsigned,
+datfile blob,
+foreign key (userid) references user(userid)
+);
+
+create table history(histid int unsigned auto_increment,
+userid int unsigned,
+temp smallint,
+date date,
+time time,
+passed boolean,
+primary key(histid),
+foreign key(userid) references user(userid)
+);

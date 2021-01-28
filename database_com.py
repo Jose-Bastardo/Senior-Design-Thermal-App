55e893811196de9626ff48ceb9034ab532bdaa8d
MajorZap <josebastardo123@gmail.com>
2021-01-28 13:21:23 -0500

    Added Comments on DB file

--- database_com.py
+++ database_com.py
@@ -1,31 +1,27 @@
 import mariadb
 
-file = open("Face_Recognition_Test/dat/23851874046344168675.dat", "rb")
-binary = file.read()
-connection = mariadb.connect(user="admin", password="2901567j", database="test", host="thermal-app.ckyrcuyndxij.us-east-2.rds.amazonaws.com")
+#file = open("Face_Recognition_Test/dat/23851874046344168675.dat", "rb")
+#binary = file.read()
+connection = mariadb.connect(user="admin", password="2901567j", database="thermalapp", host="thermal-app.ckyrcuyndxij.us-east-2.rds.amazonaws.com")
 
 cursor = connection.cursor()
 
-cursor.execute("DROP TABLE IF EXISTS user")
-cursor.execute("CREATE TABLE user(id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,"
-               "first_name VARCHAR(100), last_name VARCHAR(100), email VARCHAR(100), password VARCHAR(100), dat BLOB)")
+cursor.execute("INSERT INTO user(firstname, lastname, email, password) VALUES (?,?,?,?)",
+               ("John", "Smith", "johnsmith@hotmail.com", "passwordtest"))
+#file.close()
 
-cursor.execute("INSERT INTO user(first_name, last_name, email, password, dat) VALUES (?,?,?,?,?)",
-               ("John", "Smith", "johnsmith@hotmail.com", "passwordtest", binary))
-file.close()
+# retrieve data from user table
+cursor.execute("SELECT userid, firstname, lastname, email, password FROM user")
 
-# retrieve data
-cursor.execute("SELECT id, first_name, last_name, email, password, dat FROM user")
-
-# print content
-row= cursor.fetchone()
+# different ways of printing dataprint data
+row = cursor.fetchone()
 print(row)
 print(*row, sep='\t')
 print(row[1])
 
-file = open("Face_Recognition_Test/extract/test.dat", "wb")
-file.write(row[5])
-file.close()
+#file = open("Face_Recognition_Test/extract/test.dat", "wb")
+#file.write(row[5])
+#file.close()
 
 # free resources
 cursor.close()

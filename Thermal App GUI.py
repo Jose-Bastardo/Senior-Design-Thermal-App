dede899fa6abe50f04041b7095eef834af4fae6a
MajorZap <josebastardo123@gmail.com>
2021-02-02 14:54:30 -0500

    organized imported library code

--- Thermal App GUI.py
+++ Thermal App GUI.py
@@ -1,11 +1,13 @@
 import kivy
 import mariadb
+import dbfunctions
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
-import time
 from kivy.config import Config
 from kivy.core.window import Window
+import time
+
 Window.fullscreen = True
 
 Builder.load_string('''

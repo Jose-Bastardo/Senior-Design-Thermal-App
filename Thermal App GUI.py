9e691b3b1ecd77cc8335f0060f53f5edaea466e0
MajorZap <josebastardo123@gmail.com>
2021-02-12 13:19:32 -0500

    Added Face Rec file creation
    Added the creation of a facial recognition file when a screenshot of the camera is captured by pressing the capture button.

--- Thermal App GUI.py
+++ Thermal App GUI.py
@@ -7,6 +7,13 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.config import Config
 from kivy.core.window import Window
 import time
+import random
+from datetime import datetime
+import sys
+import face_recognition
+import pickle
+import os
+from os import path
 
 Window.fullscreen = True
 
@@ -31,10 +38,86 @@ class CameraClick(BoxLayout):
         Function to capture the images and give them the names
         according to their captured time and date.
         '''
+        logFile = open("log.txt", "a")
+        sys.stdout = logFile
+        print("========================================================================================")
+        print("----------------------------------------------------------------------------------------")
+
         camera = self.ids['camera']
         timestr = time.strftime("%Y%m%d_%H%M%S")
         camera.export_to_png("Face_Recognition/images/IMG_{}.png".format(timestr))
         print("Captured")
+
+        uploadedImageLoc = 'Face_Recognition/images/'
+        datFolderLoc = 'Face_Recognition/dat/'
+
+        # Create random number based on specified length of n
+        def randomNum(n):
+            min = pow(10, n - 1)
+            max = pow(10, n) - 1
+            return random.randint(min, max)
+
+        # Create or open log.txt to be written
+        print("\n\n========================================================================================")
+        print("----------------------------------------------------------------------------------------")
+        print("Running uploaded images' patterns extraction")
+        # Print out date and time of code execution
+        todayDate = datetime.now()
+        print("Date and time of code execution:", todayDate.strftime("%m/%d/%Y %I:%M:%S %p"))
+        print("----------------------------------------------------------------------------------------")
+
+        # Check if upload folder directory is valid
+        if (path.exists(uploadedImageLoc) == False):
+            print('Folder path does not exists. Terminating program....')
+            sys.exit()
+
+        else:
+            # For loop to read images on uploadedImages folder
+            for imageFile in os.listdir(uploadedImageLoc):
+                print("Loading image:", imageFile)
+
+                # checks if the file does exists
+                if os.path.isfile(uploadedImageLoc + imageFile):
+                    # print("File located! Executing facial pattern extraction")
+
+                    # start of encoding
+                    known_image = face_recognition.load_image_file(uploadedImageLoc + imageFile)
+                    try:
+                        known_face_encoding = face_recognition.face_encodings(known_image)[0]
+                    except IndexError:
+                        print(
+                            "I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+                        sys.stdout.close()
+                        sys.exit()
+
+                    known_faces = [
+                        known_face_encoding
+                    ]
+
+                    # Save encoding as .dat file
+                    fileLength = len(imageFile) - 4
+                    datExtension = ".dat"
+                    if (fileLength <= 0):
+                        print("ERROR! File naming failed! Terminating program....")
+                        sys.stdout.close()
+                        sys.exit()
+
+                    else:
+                        # Generate a random 20 number code for reservation ID
+                        datFileName = str(randomNum(20))
+                        with open(datFolderLoc + datFileName + datExtension, 'wb') as f:
+                            pickle.dump(known_faces, f)
+                            print("Saved pattern data as " + datFileName + " under dat folder.")
+                        os.remove(uploadedImageLoc + imageFile)
+                        # Send number to the hotel database
+                        # OR, do this after booking
+
+                else:
+                    print("Image doesn't exist! Terminating program....")
+                    print("========================================================================================")
+                    sys.exit()
+            print("SUCCESS: Patterns extraction completed. Terminiating program.")
+            print("========================================================================================")
 
 
 class TestCamera(App):
